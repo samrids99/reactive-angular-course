fb99fec654742c03dbf27fc67afa4aa5e2b65d0c
samrids99 <samridsdale108@gmail.com>
2024-08-22 16:08:41 +0100

    using a presentational component to strip unnecessary html from smart components (home)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -20,7 +20,7 @@ export class HomeComponent implements OnInit {
   advancedCourses$: Observable<Course[]>;
 
 
-  constructor(private coursesService: CoursesService, private dialog: MatDialog) {
+  constructor(private coursesService: CoursesService) {
 
   }
 
@@ -41,20 +41,7 @@ export class HomeComponent implements OnInit {
 
 
 
-  editCourse(course: Course) {
-
-    const dialogConfig = new MatDialogConfig();
-
-    dialogConfig.disableClose = true;
-    dialogConfig.autoFocus = true;
-    dialogConfig.width = "400px";
-
-    dialogConfig.data = course;
-
-    const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig);
-
-  }
-
+  
 }
 
 
