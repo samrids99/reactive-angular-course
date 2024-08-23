6387f069751cb4c4808a0982f94740460846847a
samrids99 <samridsdale108@gmail.com>
2024-08-23 09:38:40 +0100

    laoding service reactive api design, lesson 13

--- src/app/loading/loading.service.ts
+++ src/app/loading/loading.service.ts
@@ -1,6 +1,23 @@
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 
 @Injectable()
 export class LoadingService {
     constructor() { }
+
+    loading$: Observable<boolean>;  // true when loading, false when not loading
+
+    showLoaderUntilCompleted<T>(obs$: Observable<T>): Observable<T> {
+        return undefined;
+    }
+
+    loadingOn() {
+
+    }
+
+    loadingOff() {
+
+    }
+
+
 }
