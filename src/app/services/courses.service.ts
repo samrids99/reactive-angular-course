e1353885cb375f14d7ffcba252a0a01192dac872
samrids99 <samridsdale108@gmail.com>
2024-08-23 15:11:43 +0100

    angular master detail implementation (search functions), lesson 31

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -3,31 +3,45 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { Course } from "../model/course";
 import { map, shareReplay } from "rxjs/operators";
+import { Lesson } from "../model/lesson";
 
 
 @Injectable({
     providedIn: 'root'
 })
 export class CoursesService {
-  
+
 
     constructor(private http: HttpClient) {
-  
+
     }
 
     loadAllCourses(): Observable<Course[]> {
         return this.http.get<Course[]>('/api/courses')
-        .pipe(
+            .pipe(
+                map(res => res["payload"]),
+                shareReplay()
+            );
+    }
+
+    saveCourse(courseId: string, changes: Partial<Course>): Observable<any> {
+        return this.http.put(`/api/courses/${courseId}`, changes)
+            .pipe(
+                shareReplay()
+            );
+
+    }
+
+    searchLessons(search: string): Observable<Lesson[]> {
+        return this.http.get<Lesson[]>('/api/lessons', {
+            params: {
+                filter: search,
+                pageSize: "100"
+            }
+        }).pipe(
             map(res => res["payload"]),
             shareReplay()
         );
     }
 
-    saveCourse(courseId: string, changes: Partial<Course>): Observable<any> {
-        return this.http.put(`/api/courses/${courseId}`, changes)
-        .pipe(
-            shareReplay()
-        );
-
-    }
 }
