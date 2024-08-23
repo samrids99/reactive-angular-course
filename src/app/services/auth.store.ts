6f32683d8f94126d5ce22113471c5f74af1b0d07
samrids99 <samridsdale108@gmail.com>
2024-08-23 14:49:34 +0100

    user login data persistance implemented using local storage

--- src/app/services/auth.store.ts
+++ src/app/services/auth.store.ts
@@ -4,6 +4,7 @@ import { User } from "../model/user";
 import { map, share, shareReplay, tap } from "rxjs/operators";
 import { HttpClient } from "@angular/common/http";
 
+const AUTH_DATA = "auth_data"; // this is the key for the local storage
 
 @Injectable({
     providedIn: 'root'
@@ -19,18 +20,28 @@ export class AuthStore {
     constructor(private http: HttpClient) {
         this.isLoggedIn$ = this.user$.pipe(map(user => !!user));
         this.isLoggedOut$ = this.isLoggedIn$.pipe(map(loggedIn => !loggedIn));
+
+        const user = localStorage.getItem(AUTH_DATA);
+
+        if (user) {
+            this.subject.next(JSON.parse(user)); // creates an in memory user object and updates user observable
+        }
     }
 
     login(email: string, password: string): Observable<User> {
         return this.http.post<User>('api/login', { email, password }) // here email and password is a json payload
             .pipe(
-                tap(user => this.subject.next(user)),
+                tap(user => {
+                    this.subject.next(user);
+                    localStorage.setItem(AUTH_DATA, JSON.stringify(user)); // store the user in the local storage (local storage only stores strings)
+                }),
                 shareReplay()
             );
     }
 
     logout() {
         this.subject.next(null); // needs to emit the null value again (back to original state)
+        localStorage.removeItem(AUTH_DATA); // clears the local storage
     }
 
 }
